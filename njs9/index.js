bd7a3efd26da46aed7623d07dccba8deabf095e5
Avi <avi@localhost.localdomain>
2019-08-06 13:37:55 +0530

    Nodexpress njs9

---
+++ njs9/index.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const csrf = require('csurf')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const csrfProtect = csrf({cookie: true})
+const formParser = bodyParser.urlencoded({extended: false})
+
+const app = express()
+
+app.set('view engine', 'pug')
+app.set('views', 'view-files')
+
+app.use(cookieParser())
+
+app.get('/', csrfProtect, function (req, res) {
+  console.log('Token to Browser/form: ' + req.csrfToken());
+  res.render('form.pug', {csrfToken: req.csrfToken()})
+})
+
+app.post('/response', formParser, csrfProtect, function(req, res) {
+  console.log('Token from Browser/form: ' + req.body._csrf)
+  // console.log(req.body);
+  res.send("data is authenticated");
+})
+
+app.listen(8000)
+
+
+
+
+
+
